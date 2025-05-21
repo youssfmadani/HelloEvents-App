94f3168eb17a2df159ae92ea108aecdd2f629cff
youssfmadani <youssfelmadani@gmail.com>
2025-05-21 11:16:32 +0100

    La configuration de projet

---
+++ src/main/java/com/example/helloevents/HelloEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloEventsApplication.class, args);
+	}
+
+}
